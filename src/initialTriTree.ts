2c5d2c41755e1e2570d3ed18017baa16f59c6ef6
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-01-27 00:45:47 +0100

    refactor(initialTriTree): generate with tools

--- src/initialTriTree.ts
+++ src/initialTriTree.ts
@@ -1,40 +1,13 @@
 import { TriTree } from './types';
 
-export const initialTriTree: TriTree = {
-  divided: true,
-  children: {
-    mid: {
-      divided: true,
-      children: {
-        mid: {
-          divided: false,
-          orientation: 0,
-        },
-        left: {
-          divided: false,
-          orientation: 0,
-        },
-        right: {
-          divided: false,
-          orientation: 0,
-        },
-        top: {
-          divided: false,
-          orientation: 0,
-        },
-      },
-    },
-    left: {
-      divided: false,
-      orientation: 0,
-    },
-    right: {
-      divided: false,
-      orientation: 0,
-    },
-    top: {
-      divided: false,
-      orientation: 0,
-    },
-  },
+import { tools } from './tools';
+
+const baseTriTree: TriTree = {
+  divided: false,
+  orientation: 0,
 };
+
+export const initialTriTree: TriTree = tools.subdivide(
+  tools.subdivide(baseTriTree, []),
+  ['mid'],
+);
