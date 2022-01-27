254310f54649d137eabfd36b2722093582fb121a
bxt <bxt@die-optimisten.net>
2022-01-27 00:16:12 +0000

    Deploying to gh-pages from  @ 0833021cecd5c3c2c656d46b70219e3a888f9b59 🚀

--- _next/static/K2j-ALJ6BAe8I_dZDirPC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1498a76d2a1ffcaf.css","static/chunks/pages/index-8bd70f8b72a1b94d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ofkAOdoM55h-EAr1dZ_AJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4449f41f0b964c22.css","static/chunks/pages/index-6d6fb5728a3a9134.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
