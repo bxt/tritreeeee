0bb5d329c0516fb40047c108f9fd2cf7f41e703e
Bernhard Häussner <bernhard.haeussner@xing.com>
2022-03-06 19:40:27 +0100

    feat: add now color theme

--- src/themes.tsx
+++ src/themes.tsx
@@ -5,8 +5,8 @@ const THEME_COLOR_COUNT = 8;
 export const getColor = (number: number) =>
   `var(--color${number % THEME_COLOR_COUNT})`;
 
-const { grayscale, blackpink, pastels } = styles;
+const { grayscale, blackpink, pastels, ukraine } = styles;
 
-export const themes = { grayscale, blackpink, pastels } as const;
+export const themes = { grayscale, blackpink, pastels, ukraine } as const;
 
 export type ThemeName = keyof typeof themes;
